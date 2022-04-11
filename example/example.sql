736e0b5d2290861be7a6f11f65187e639abd8bea
Clement HERVE <HERVEC>
2022-04-11 10:42:38 +0200

    updated the example.sql

--- example/example.sql
+++ example/example.sql
@@ -1,13 +1,13 @@
-drop table if exists 'example';
-create table if not exists 'example' (
-	A int,
-	B int,
-	C int,
-	D text
-);
-insert into 'example' 
-	('A', 'B', 'C', 'D'),
-values
-	('0', '1', '2', 'hey'),
-	('1', '1', '3', 'hoy'),
-	('2', '2', '5', 'h\'ay');
+drop table if exists `example`; 
+create table if not exists `example` (
+	`A` int, 
+	`B` int, 
+	`C` int, 
+	`D` text
+); 
+insert into `example` 
+	(`A`, `B`, `C`, `D`)
+ values 
+	(0, 1, 2, 'hey'), 
+	(1, 1, 3, 'hoy'), 
+	(2, 2, 5, 'h\'ay');
